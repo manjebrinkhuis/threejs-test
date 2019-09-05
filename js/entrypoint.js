404f1230a8ea66b8c900626d31312f6d28353f2c
Manje Brinkhuis <manjebrinkhuis@gmail.com>
2019-09-05 15:16:42 +0700

    3D object to http://

--- js/entrypoint.js
+++ js/entrypoint.js
@@ -1,23 +1,25 @@
 //
-var root = document.getElementById("root");
+let root = document.getElementById("root");
 root.style.width = window.innerWidth;
 root.style.height = window.innerHeight;
 
 //
-var canvas = document.createElement("canvas");
+let canvas = document.createElement("canvas");
 canvas.setAttribute("width", root.clientWidth);
 canvas.setAttribute("height", root.clientHeight);
 
 // Append canvas to document before continuing;
 root.appendChild(canvas);
-var scene = new ThreeScene(canvas);
+let scene = new ThreeScene(canvas);
 
 // obj
-var manager = new THREE.LoadingManager();
-var loader = new THREE.ObjectLoader( manager );
+let manager = new THREE.LoadingManager();
+let loader = new THREE.ObjectLoader( manager );
 
-// var cube = new Cube({color: "#567"});
-loader.load( 'http://github.com/manjebrinkhuis/brain/brain.obj', ( object ) => {
+// let cube = new Cube({color: "#567"});
+let url =  "https://raw.githubusercontent.com/manjebrinkhuis/threejs-test/master/assets/brain.obj";
+
+loader.load( url, ( object ) => {
   //
   object.traverse( ( child ) => {
     scene.addObject( child )
